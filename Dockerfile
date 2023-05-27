99b7cf393626794018ddad07705242656ed0d032
Eric P <ericpp@gmail.com>
2023-05-26 19:30:47 -0500

    Use HELIPAD_RUNAS_USER to set running user id

--- Dockerfile
+++ Dockerfile
@@ -13,7 +13,6 @@ COPY helipad /opt/helipad
 
 WORKDIR /opt/helipad
 RUN cargo build --release
-RUN ls -la
 RUN cp target/release/helipad .
 
 ##: Bundle stage
@@ -24,22 +23,20 @@ ARG PLATFORM
 
 USER root
 
-RUN apt-get update && apt-get install -y apt-utils ca-certificates tini sqlite3 openssl
-
-RUN chown -R 1000:1000 /opt
-RUN mkdir /data && chown -R 1000:1000 /data
+RUN apt-get update && apt-get install -y apt-utils ca-certificates openssl sqlite3 tini
 
 COPY ./docker_entrypoint.sh /usr/local/bin/docker_entrypoint.sh
 COPY ./check-web.sh /usr/local/bin/check-web.sh
 RUN chmod +x /usr/local/bin/*.sh
 
-#USER 1000
 RUN mkdir /opt/helipad
 
 WORKDIR /opt/helipad
 COPY --from=builder /opt/helipad/target/release/helipad .
 COPY --from=builder /opt/helipad/webroot ./webroot
 COPY --from=builder /opt/helipad/helipad.conf .
+
+RUN useradd -u 1000 helipad
 
 EXPOSE 2112/tcp
 
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-export HELIPAD_DATABASE_DIR="/app/data/database.db"
+export HELIPAD_DATABASE_DIR="/data/db/database.db"
 export HELIPAD_LISTEN_PORT="2112"
+export HELIPAD_RUNAS_USER="1000"
 export LND_ADMINMACAROON="/mnt/lnd/admin.macaroon"
 export LND_TLSCERT="/mnt/lnd/tls.cert"
 export LND_URL="lnd.embassy:10009"
@@ -13,11 +14,20 @@ if ! [ -f $LND_ADMINMACAROON ]; then
   exit 1
 fi
 
-echo "Trusting Embassy OS certificate"
-cd /usr/local/share/ca-certificates/
-awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".crt"}' < /mnt/lnd/tls.cert
-update-ca-certificates
+if ! [ -d "/data/db" ]; then
+  echo "Creating database directory"
+  mkdir -p /data/db
+  chown -v 1000:1000 /data/db
+fi
+
+if ! [ -f "/usr/local/share/ca-certificates/cert.crt" ]; then
+  echo "Trusting Start OS certificate"
+  cd /usr/local/share/ca-certificates/
+  awk 'split_after==1{n++;split_after=0} /-----END CERTIFICATE-----/ {split_after=1} {print > "cert" n ".crt"}' < /mnt/lnd/tls.cert
+  update-ca-certificates
+fi
 
 echo "Starting Helipad..."
 cd /opt/helipad
+
 exec tini -g -- /opt/helipad/helipad
