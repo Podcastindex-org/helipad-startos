f19863d01c9c74655869aa22f8061fea4c5ca205
Eric P <ericpp@gmail.com>
2023-09-27 21:58:15 -0500

    No config currently needed for package

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -1,5 +1,3 @@
-export { setConfig } from "./services/setConfig.ts";
 export { properties } from "./services/properties.ts";
-export { getConfig } from "./services/getConfig.ts";
 export { health } from "./services/healthChecks.ts";
 export { migration } from "./services/migrations.ts";
