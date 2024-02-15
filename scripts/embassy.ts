06f0a4626e8c0344a8b085eca01ad92c72f06fc4
Eric P <ericpp@gmail.com>
2024-02-15 00:45:50 -0600

    Updated embassy-sdk references to start-sdk

--- scripts/embassy.ts
+++ scripts/embassy.ts
@@ -1,5 +1,5 @@
-export { getConfig } from "./services/getConfig.ts";
-export { setConfig } from "./services/setConfig.ts";
-export { properties } from "./services/properties.ts";
-export { health } from "./services/healthChecks.ts";
-export { migration } from "./services/migrations.ts";
+export { getConfig } from "./procedures/getConfig.ts";
+export { setConfig } from "./procedures/setConfig.ts";
+export { properties } from "./procedures/properties.ts";
+export { health } from "./procedures/healthChecks.ts";
+export { migration } from "./procedures/migrations.ts";
